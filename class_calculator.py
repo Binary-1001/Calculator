2a142ec97438c57e8365581968c8544002b804b0
nhmokhocjc025 <nhmokhocjc025@student.wethinkcode.co.za>
2025-12-11 01:42:05 +0200

    Added tests.

---
+++ class_calculator.py
@@ -0,0 +1,19 @@
+class Calculator:
+    def __init__(self,x,y):
+        pass
+
+
+    def add(self):
+        pass
+
+
+    def subtract(self):
+        pass
+
+
+    def multiply(self):
+        pass
+
+
+    def divide(self):
+        pass
---
+++ tests/test_class_calculator.py
@@ -0,0 +1,28 @@
+import unittest
+
+from class_calculator import *
+
+class TestClassCalculator(unittest.TestCase):
+    
+    def test_add(self):
+        sum = Calculator(1,2)
+        self.assertEqual(3,sum.add())
+
+    def test_subtract(self):
+        difference = Calculator(5,2)
+        self.assertEqual(3,difference.add())
+        
+
+    def test_multiply(self):
+        product = Calculator(1,2)
+        self.assertEqual(2,product.add())
+
+    def test_divide(self):
+        quotient = Calculator(1,2)
+        self.assertEqual(0.5,quotient.add())
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
